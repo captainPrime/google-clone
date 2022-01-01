2996deea65eb6561f7d40fe0ae06dc9e414cc542
taiwo <taiwooyindamola515@gmail.com>
2022-01-01 10:43:26 +0100

    updated services

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import SearchPage from './pages/SearchPage';
-import Handlinghttp from './pages/handlinghttp';
 import './App.css';
+
 
 function App() {
     return (
         <div className="App">
             <SearchPage />
-            <Handlinghttp />
         </div>
     );
 }
