f46da240dde1d29f03fbe42e49c63ceca291ddca
taiwo <taiwooyindamola515@gmail.com>
2022-01-02 18:27:29 +0100

    added functionalities

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { db } from '../services/firebase-config';
 import { FaTwitter } from 'react-icons/fa';
 import '../assets/css/bookmark.css';
 import '../assets/css/search.css';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { getDocument, createDocument } from '../services/firebase-config';
 import Bookmarks from '../components/bookmarks';
 import Search from '../components/search';
 import Logo from '../components/logo'
@@ -19,14 +18,13 @@ interface searchInput {
 }
 
 function SearchPage() {
+    const [searchValue, setSearchValue] = useState('');
     const [bookmarkFromStore, setBookmarks] = useState<BookmarkType[]>([]);
 
-    // firebase store
-    const urlCollectionRef = collection(db, 'url');
     //--------------------------------------
     useEffect(() => {
-        const getUrl = async () => {
-            const data = await getDocs(urlCollectionRef);
+        const getUrl = async () => { 
+            const data = await getDocument('url');
             setBookmarks(
                 data.docs.map((doc) => ({ name: doc.data().name, id: doc.id })),
             );
@@ -35,24 +33,24 @@ function SearchPage() {
         getUrl();
     }, []);
 
-    const [searchValue, setSearchValue] = useState('');
+
 
     const handlechangeFn = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.target.value);
     };
 
-    const CreateUrlFnc = () => {
-        const createUrl = async () => {
-            try {
-                await addDoc(urlCollectionRef, { name: searchValue });
-                window.open(`https://${searchValue}`);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        createUrl();
-    };
+     const CreateUrlFnc = () => {
+          const createUrl = async () => {
+              try {
+                  await createDocument('url', { name: searchValue});
+                  window.open(`https://${searchValue}`);
+              } catch (error) {
+                  console.log(error);
+              }
+          };
+  
+          createUrl();
+      }; 
 
     return (
         <div>
@@ -60,7 +58,7 @@ function SearchPage() {
             <Search
                 CreateUrlFnc={CreateUrlFnc}
                 handlechange={handlechangeFn}
-            />
+            /> 
             <Bookmarks bookmarks={bookmarkFromStore} />
         </div>
     );
@@ -68,47 +66,3 @@ function SearchPage() {
 
 export default SearchPage;
 
-
-     // saving background url
-
-/*  if(imageAsUrlUpload !== "") {
-const createUrl = async( ) => {
-try {
-    await addDoc(urlCollectionRef, {name: imageAsUrlUpload})
-} catch (error) {
-    console.log(error)
-}
-
- 
-}
-
-createUrl()
-
-} 
-
-//handles image upload to firebase
-/*
-if(!imageAsFile ) {
-console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
-alert("Please select a valid file")
-} 
- 
-const uploadTask = await storage.ref(`/images/${imageAsFile?.name}`).put(imageAsFile) 
- 
-//initiates the firebase side uploading 
-uploadTask.on('state_change', (snapShot:any) => {
-//takes a snap shot of the process as it is happening
-console.log(snapShot)
-} , (err) => {
-//catches the errors
-console.log(err)
-} , () => {
-// gets the functions from storage refences the image storage in firebase by the children
-// gets the download url then sets the image from firebase as the value for the imgUrl key:
-
-storage.ref('images').child(imageAsFile.name).getDownloadURL()
-.then(fireBaseUrl => {
-  setImageAsUrl(prevObject => ({...prevObject, imgUrl: fireBaseUrl}))
-})
-
-}) */
--- src/services/firebase-config.tsx
+++ src/services/firebase-config.tsx
@@ -1,7 +1,20 @@
 import  {initializeApp } from "firebase/app"
 import { getFirestore } from "@firebase/firestore"
-const firebaseConfig = {
- 
-  };
-const app = initializeApp(firebaseConfig);
+import { collection, getDocs, addDoc } from 'firebase/firestore';
+
+import config from "../utils/config";
+
+interface payloadFormat  {
+    name: string
+}
+
+const app = initializeApp(config);
 export const db = getFirestore(app);  
+
+export const getDocument = async (name: string) => {
+  return await getDocs(collection(db, name));
+} 
+
+export const createDocument = async (name: string, payload: payloadFormat  ) => {
+  return await addDoc(collection(db, name), payload )
+}
